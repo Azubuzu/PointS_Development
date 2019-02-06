1da64c4cb818ca1040374de58ce56e044c8aa284
Azubuzu <azuria.ng@gmail.com>
2019-02-06 13:00:40 +0100

    Upload main files

---
+++ js/lang.js
@@ -0,0 +1,25 @@
+var langs = ['en','fr','it'];
+var langCode = '';
+var langJS = null;
+
+
+var translate = function (jsdata)
+{	
+	$("[tkey]").each (function (index)
+	{
+		var strTr = jsdata [$(this).attr ('tkey')];
+	    $(this).html (strTr);
+	});
+}
+
+
+//langCode = navigator.language.substr (0, 2);
+langCode = 'fr';
+
+if (langCode in langs)
+	$.getJSON('lang/'+langCode+'.json', translate);
+else
+	$.getJSON('lang/fr.json', translate);
+
+
+
